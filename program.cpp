04a485188666dca2c6a15da07bb747cc17c1ae84
Zod <48190910+Zod-24@users.noreply.github.com>
2019-07-12 21:04:16 -0700

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int secsertNum = 7;
+  int guess;
+  int guessCount = 0;
+  int guessLimit = 3;
+  bool outOfGuesses = false;
+
+  while (secsertNum != guess && !outOfGuesses) {
+    if(guessCount < guessLimit) {
+      cout << "Enter guess: ";
+      cin >> guess;
+      guessCount++;
+    } else {
+      outOfGuesses = true;
+    }
+  }
+
+  if(outOfGuesses) {
+    cout << "You Lose!";
+  } else {
+    cout << "You Win!";
+  }
+
+  return 0;
+}
